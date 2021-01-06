2262332675cc93faf2275e418d2230dd15b04424
Олег Хейн <okhiein@mail.ru>
2021-01-06 20:41:06 +0500

    just test1

--- SQL/delete.py
+++ SQL/delete.py
@@ -2,15 +2,15 @@ import sqlite3 as sl
 
 con = sl.connect('inter.db')
 cur = sl.Cursor
-#DELETE FROM table_name 
-#WHERE name='Ford';
+# DELETE FROM table_name
+# WHERE name='Ford';
 
 sql = "DELETE FROM USER WHERE name = 'Alice'"
 
-#sql = 'INSERT INTO USER (id, name, age, email, mobile) values(?, ?, ?, ?, ?)'
+# sql = 'INSERT INTO USER (id, name, age, email, mobile) values(?, ?, ?, ?, ?)'
 
-#cur.execute("DELETE FROM USER WHERE name='Alice';")
-#conn.commit()
+# cur.execute("DELETE FROM USER WHERE name='Alice';")
+# conn.commit()
 
-#with con:
+# with con:
 #    con.executemany(sql, data)
